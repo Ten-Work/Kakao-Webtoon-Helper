0ad51496b5f11b79fdcec8ca53e2dc984e3b878e
Kate Lee <xoolwater@gmail.com>
2025-01-22 20:14:57 +0800

    加上編號

--- result.js
+++ result.js
@@ -3,9 +3,10 @@
 const data = JSON.parse(localStorage.getItem("scrapedData"));
 
 const tableBody = document.querySelector('#result-table tbody');
-data.forEach(item => {
+data.forEach((item, index) => {
     const row = document.createElement('tr');
     row.innerHTML = `
+    <td width="30px">${index + 1}</td>
     <td><a href="https://tw.kakaowebtoon.com${item.url}" target="_blank">${item.title}</a></td>
     <td>${item.ticketCount}張</td>
     `;
